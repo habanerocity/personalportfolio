e5bed6dbdf28b81ace351f1786eaec2139576f85
habanerocity <lindyo87@gmail.com>
2024-07-22 20:19:55 +0800

    added footer section

---
+++ components/ui/Footer.js
@@ -0,0 +1,10 @@
+import classes from './Footer.module.scss';
+function Footer() {
+    return (
+      <footer className={classes.footer__class}>
+        <p>© 2024 Lindy Ramirez, All Rights Reserved</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Hero from '../components/ui/Hero';
 
+import React from 'react';
+
 import { motion } from 'framer-motion';
 
 import NavigationBar from '../components/ui/NavigationBar';
 import LogoBar from '../components/ui/LogoBar';
+import Footer from '../components/ui/Footer';
 
 import classes from './index.module.scss';
 
@@ -11,36 +14,39 @@ import Head from 'next/head';
 
 const HomePage = () => {
 	return (
-		<main className={classes.main}>
-			<Head>
-				<meta
-					name="description"
-					content="Home page for Lindy Ramirez | Front End React Developer Based in Los Angeles, California. View my developer portfolio here."
-				/>
-			</Head>
-			<NavigationBar xw />
-			<motion.div
-				initial="hidden"
-				animate="visible"
-				className={` ${classes.motion}`}
-				variants={{
-					hidden: {
-						scale: 0.8,
-						opacity: 0
-					},
-					visible: {
-						scale: 1,
-						opacity: 1,
-						transition: {
-							delay: 0.4
+		<React.Fragment>
+			<main className={classes.main}>
+				<Head>
+					<meta
+						name="description"
+						content="Home page for Lindy Ramirez | Front End React Developer Based in Los Angeles, California. View my developer portfolio here."
+					/>
+				</Head>
+				<NavigationBar xw />
+				<motion.div
+					initial="hidden"
+					animate="visible"
+					className={` ${classes.motion}`}
+					variants={{
+						hidden: {
+							scale: 0.8,
+							opacity: 0
+						},
+						visible: {
+							scale: 1,
+							opacity: 1,
+							transition: {
+								delay: 0.4
+							}
 						}
-					}
-				}}
-			>
-				<Hero />
-			</motion.div>
-			<LogoBar xw />
-		</main>
+					}}
+				>
+					<Hero />
+				</motion.div>
+				<LogoBar xw />
+			</main>
+			<Footer />
+		</React.Fragment>
 	);
 };
 

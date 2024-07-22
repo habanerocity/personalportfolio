99d0e17a80da714197ba1da5c09e85660672cc17
habanerocity <lindyo87@gmail.com>
2024-07-23 02:32:58 +0800

    resolved conflict on inactive skills page

--- pages/Skills.js
+++ pages/Skills.js
@@ -4,7 +4,7 @@ import classes from './Skills.module.scss';
 
 import { motion } from 'framer-motion';
 
-import { skillsInfo } from '../components/data/skillsInfoData';
+import { skillsInfo } from '../components/data/toolsInfoData';
 
 import SectionContainer from '../components/ui/SectionContainer';
 import Headings from '../components/ui/Headings';

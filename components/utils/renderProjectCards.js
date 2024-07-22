cbd672161deb6ab97ffbe4a4ea340984c7cd8899
habanerocity <lindyo87@gmail.com>
2024-07-23 02:10:13 +0800

    refactored projects page and abstracted away helper functions

---
+++ components/utils/renderProjectCards.js
@@ -0,0 +1,38 @@
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+
+import classes from './renderProjectCards.module.scss';
+
+const renderProjectCards = (cardProjectData, index) => {
+    return (
+        <Card className={`${classes.card} col-12 col-sm-12 col-md-12 col-lg-5`} key={index}>
+            <Card.Img className={classes.card__top__br} variant="top" src={cardProjectData.image} alt={cardProjectData.title} />
+            <Card.Body className={`${classes.card__body} p-4 d-flex flex-column align-items-center justify-content-between`}>
+                <div className={classes.flex__card__body} >
+                    <h2 className={` ${classes.card__heading}`}>{cardProjectData.title}</h2>
+                    <ListGroup className="list-group-flush text-center ">
+                        <ListGroupItem>
+                            <h3>Tools used</h3>
+                            {cardProjectData.tools.map((tool, index) => (
+                                <Badge key={index} pill text="light" className={`p-2 fs-4 m-2 ${classes.secondary_color}`}>
+                                    <span className={classes.pill}>{tool}</span>
+                                </Badge>
+                            ))}
+                        </ListGroupItem>
+                    </ListGroup>
+                    <span className={`${classes.card__description} fs-4 my-2`}>{cardProjectData.description}</span>
+                </div>
+                <div className="w-100 mt-2 d-flex justify-content-evenly align-items-center">
+                    {cardProjectData.liveDemo && <Button href={cardProjectData.liveDemo} target="_blank" className={`fs-3`} variant="secondary" size="lg">Live Demo</Button>}
+                    {cardProjectData.github && <Button href={cardProjectData.github} target="_blank" className={`fs-3`} variant="outline-secondary" size="lg">View Code</Button>}
+                    {cardProjectData.designFile && <Button href={cardProjectData.designFile} target="_blank" className={`fs-3 px-4`} variant="secondary" size="lg">View Design</Button>}
+                </div>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default renderProjectCards;
--- pages/Projects.js
+++ pages/Projects.js
@@ -1,19 +1,14 @@
 import NavigationBar from '../components/ui/NavigationBar';
 import LogoBar from '../components/ui/LogoBar';
 import Footer from '../components/ui/Footer';
-
 import SectionContainer from '../components/ui/SectionContainer';
 import Headings from '../components/ui/Headings';
-import { cardInfo as projectInfo } from '../components/data/projectsCardInfoData';
-
-import ListGroupItem from 'react-bootstrap/ListGroupItem';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
-
 import ProjectCategories from '../components/ui/ProjectCategories';
+
+import { cardInfo as projectInfoData } from '../components/data/projectsCardInfoData';
+import renderProjectCards from '../components/utils/renderProjectCards';
+
+import Container from 'react-bootstrap/Container';
 
 import pageFadeInAnimation from '../components/animations/pageFadeInAnimation';
 import cardFadeInAnimation from '../components/animations/cardFadeInAnimation';
@@ -32,35 +27,6 @@ const Projects = () => {
 	const handleCategoryClick = (category) => {
 		setActiveCategory(category)
 	}
-
-	const renderCard = (card, index) => {
-		return (
-			<Card className={`${classes.card} col-12 col-sm-12 col-md-12 col-lg-5`} key={index}>
-				<Card.Img className={classes.card__top__br} variant="top" src={card.image} alt={card.title} />
-				<Card.Body className={`${classes.card__body} p-4 d-flex flex-column align-items-center justify-content-between`}>
-					<div className={classes.flex__card__body} >
-						<h2 className={` ${classes.card__heading}`}>{card.title}</h2>
-						<ListGroup className="list-group-flush text-center ">
-							<ListGroupItem>
-								<h3>Tools used</h3>
-								{card.tools.map((tool, index) => (
-									<Badge key={index} pill text="light" className={`p-2 fs-4 m-2 ${classes.secondary_color}`}>
-										<span className={classes.pill}>{tool}</span>
-									</Badge>
-								))}
-							</ListGroupItem>
-						</ListGroup>
-						<span className={` ${classes.card__description} fs-4 my-2`}>{card.description}</span>
-					</div>
-					<div className="w-100 mt-2 d-flex justify-content-evenly align-items-center">
-						{card.liveDemo && <Button href={card.liveDemo} target="_blank" className={`fs-3`} variant="secondary" size="lg">Live Demo</Button>}
-						{card.github && <Button href={card.github} target="_blank" className={`fs-3`} variant="outline-secondary" size="lg">View Code</Button>}
-						{card.designFile && <Button href={card.designFile} target="_blank" className={`fs-3 px-4`} variant="secondary" size="lg">View Design</Button>}
-					</div>
-				</Card.Body>
-			</Card>
-		);
-	};
 
 	return (
 		<React.Fragment>
@@ -95,8 +61,8 @@ const Projects = () => {
 							animate="visible"  
 							className={`${classes.card__holder}`}
 							>
-							{projectInfo.filter(project => activeCategory === 'All' || project.categories.includes(activeCategory))
-							.map((project, index) => renderCard(project, index))
+							{projectInfoData.filter(project => activeCategory === 'All' || project.categories.includes(activeCategory))
+							.map((project, index) => renderProjectCards(project, index))
 							}
 							</motion.div>
 						</Container>

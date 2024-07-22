3da2b0241e39296152a05382d803a18c23725d49
habanerocity <lindyo87@gmail.com>
2024-07-22 23:24:35 +0800

    renamed .details class to .bio__details for more clarity

--- pages/About.js
+++ pages/About.js
@@ -50,7 +50,7 @@ const About = () => {
               <FlexContainer>
                 <div className={`${classes.about__card} col-4`}>
                   <ProfileHeader />
-                  <div className={classes.details}>
+                  <div className={classes.bio__details}>
                     <div className="fs-4 pt-4">
                       Hey, I&apos;m Lindy, a freelance web developer deeply rooted in digital marketing and the e-commerce world. With a track record of helping brands improve their online presence and conversion rates, my journey in tech began with a childhood love for computers, evolving into a problem-solving mission. Beyond coding, I&apos;m a big fan of basketball and soccer, an adventure-seeking traveler, and a spicy food aficionado. If you&apos;re looking to collaborate on a project or just chat tech and shared interests, feel free to reach out—I&apos;m always up for new ventures.
                     </div>

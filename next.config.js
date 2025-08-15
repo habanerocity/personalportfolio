ea2796ee5dcbb1e127441818f285eed5b7e1d72c
habanerocity <lindyo87@gmail.com>
2025-08-16 05:28:37 +0800

    updated next.config file to handle redirects for failing routes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-	  unoptimized: true
-	},
-	webpack(config) {
-		config.module.rules.push({
-		test: /\.svg$/,
-		use: ["@svgr/webpack"]
-		});
-		return config;
-	}
-  };
-  
-  module.exports = nextConfig;
+  images: {
+    unoptimized: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/Contact',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/Projects', 
+        destination: '/projects',
+        permanent: true,
+      },
+    ]
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+    return config;
+  }
+};
+
+module.exports = nextConfig;

2cbab3661de59b6bbd82a267652f37dec15f8bdc
habanerocity <lindyo87@gmail.com>
2025-08-16 05:41:27 +0800

    added new build script to package.json

--- next.config.js
+++ next.config.js
@@ -1,29 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true
-  },
-  async redirects() {
-    return [
-      {
-        source: '/Contact',
-        destination: '/contact',
-        permanent: true,
-      },
-      {
-        source: '/Projects', 
-        destination: '/projects',
-        permanent: true,
-      },
-    ]
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-    return config;
-  }
-};
-
-module.exports = nextConfig;
+	images: {
+	  unoptimized: true
+	},
+	webpack(config) {
+		config.module.rules.push({
+		test: /\.svg$/,
+		use: ["@svgr/webpack"]
+		});
+		return config;
+	}
+  };
+  
+  module.exports = nextConfig;

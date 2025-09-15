4a74f059b405ca4378dcb58ce953893db3846d33
habanerocity <lindyo87@gmail.com>
2025-09-15 12:56:35 +0800

    added google analytics components and env variables

---
+++ components/utils/gtag.js
@@ -0,0 +1,44 @@
+// lib/gtag.js
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('config', GA_MEASUREMENT_ID, {
+      page_path: url,
+    });
+  }
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
+  }
+};
+
+// Enhanced ecommerce events
+export const purchase = ({ transaction_id, value, currency, items }) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', 'purchase', {
+      transaction_id: transaction_id,
+      value: value,
+      currency: currency,
+      items: items,
+    });
+  }
+};
+
+export const addToCart = ({ currency, value, items }) => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    window.gtag('event', 'add_to_cart', {
+      currency: currency,
+      value: value,
+      items: items,
+    });
+  }
+};
